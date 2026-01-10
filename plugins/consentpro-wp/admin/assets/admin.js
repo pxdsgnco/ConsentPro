c5ed52bd6083835afab8f12544aacf2a3fcc918a
Adedoyin Kassem <doyin.kassem@gmail.com>
2026-01-09 23:35:07 -0500

    style: fix Prettier formatting in admin.js

--- plugins/consentpro-wp/admin/assets/admin.js
+++ plugins/consentpro-wp/admin/assets/admin.js
@@ -6,31 +6,31 @@
 
 /* global jQuery */
 (function ($) {
-	'use strict';
+  'use strict';
 
-	/**
-	 * Initialize WordPress color pickers on the Appearance tab.
-	 */
-	function initColorPickers() {
-		$('.consentpro-color-field').each(function () {
-			var $field = $(this);
+  /**
+   * Initialize WordPress color pickers on the Appearance tab.
+   */
+  function initColorPickers() {
+    $('.consentpro-color-field').each(function () {
+      var $field = $(this);
 
-			if (typeof $.fn.wpColorPicker !== 'undefined') {
-				$field.wpColorPicker({
-					defaultColor: $field.data('default') || false,
-					change: function () {
-						// Color changed - could add live preview here.
-					},
-					clear: function () {
-						// Color cleared - reset to default.
-					}
-				});
-			}
-		});
-	}
+      if (typeof $.fn.wpColorPicker !== 'undefined') {
+        $field.wpColorPicker({
+          defaultColor: $field.data('default') || false,
+          change: function () {
+            // Color changed - could add live preview here.
+          },
+          clear: function () {
+            // Color cleared - reset to default.
+          },
+        });
+      }
+    });
+  }
 
-	// Initialize on DOM ready.
-	$(document).ready(function () {
-		initColorPickers();
-	});
+  // Initialize on DOM ready.
+  $(document).ready(function () {
+    initColorPickers();
+  });
 })(jQuery);
